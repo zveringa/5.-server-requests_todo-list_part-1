c3095935319b7e2ac220ce340c5a3142954b4011
Eugene Zveringa <zveringa@gmail.com>
2024-05-28 23:16:27 +0100

    part-2-get

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
 import styles from './app.module.css';
 import { useState, useEffect } from 'react';
 
-const TODO_LIST = [
-	{
-		id: '001',
-		task: 'Реализовать приложение на базе Create React App — страницу со списком дел (Todo list)',
-	},
-	{
-		id: '002',
-		task: 'Переделать приложение, заменив JSON Placeholder на JSON Server',
-	},
-	{
-		id: '003',
-		task: 'Также дополнительно — сделать приложение из второго пункта, но с использованием Firebase (без использования JSON Server)',
-	},
-];
-
 export const App = () => {
 	const [todos, setTodos] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
-		new Promise((resolve) => {
-			resolve({ json: () => TODO_LIST });
-		})
+		setIsLoading(true);
+		fetch('http://localhost:3001/todos')
 			.then((loadedData) => loadedData.json())
 			.then((loadedTodos) => {
 				setTodos(loadedTodos);
-			});
+			})
+			.finally(() => setIsLoading(false));
 	}, []);
 
 	return (
 		<div className={styles.app}>
-			{todos.map(({ id, task }) => (
-				<div key={id}>
-					{' '}
-					#{id}: - {task}{' '}
-				</div>
-			))}
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				todos.map(({ id, task }) => (
+					<div key={id}>
+						{' '}
+						#{id}: - {task}{' '}
+					</div>
+				))
+			)}
 		</div>
 	);
 };
