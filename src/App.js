9a6a41b26c161a6792777902e08d76b77d690ec7
Eugene Zveringa <Eugene.Zveringa@tenon-eng.com>
2024-06-05 17:01:23 +0100

    part-1 final

--- src/App.js
+++ src/App.js
@@ -4,44 +4,33 @@ import { useState, useEffect } from 'react';
 export const App = () => {
 	const [todos, setTodos] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
-	const
 
-		useEffect(() => {
-			setIsLoading(true);
-			fetch('http://localhost:3001/todos')
-				.then((loadedData) => loadedData.json())
-				.then((loadedTodos) => {
-					setTodos(loadedTodos);
-				})
-				.finally(() => setIsLoading(false));
-		}, []);
-
-	const addTask = () => {
-		fetch('http://localhost:3001/todos', {
-			method: 'POST',
-			header: { 'Content-Type': 'application/json;charset=utf-8' },
-			body: JSON.stringify({
-				task: { newTask }
-			}),
-		});
-	}
+	useEffect(() => {
+		setIsLoading(true);
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((loadedData) => loadedData.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
 
 	return (
 
 		<div className={styles.app}>
 			<div className={styles.header}>Здесь твой список дел:</div>
-			<form onSubmit={handleSubmit(onSubmit)}></form>
-			<button className={styles.button} onClick={addTask}>Добавить дело</button>
+
+
 			{isLoading ? (
 				<>
 					<div className={styles.headingLoader}>Please wait, the page is loading!</div>
 					<div className={styles.loader}></div>
 				</>
 			) : (
-				todos.map(({ id, task }) => (
-					<div key={id}>
-						{' '}
-						#{id}: - {task}{' '}
+				todos.map(({ id, title, completed }) => (
+					<div key={id} className={styles.list}>
+						<input className={styles.checkbox} type='checkbox' checked={completed} readOnly />
+						# {id}: - {title}
 					</div>
 				))
 			)}
